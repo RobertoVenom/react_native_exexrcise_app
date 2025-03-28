69af3e13b8884e13afd56cea9ec2e044950d9c1f
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2025-03-28 08:13:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import Navigation from './src/Navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/screens/Home';
+import RepetitionExercise from './src/screens/RepetitionExercise';
+import DurationExercise from './src/screens/DurationExercise';
 
-export default function App() {
-  return <Navigation />;
-}
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="RepetitionExercise" component={RepetitionExercise} />
+        <Stack.Screen name="DurationExercise" component={DurationExercise} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
