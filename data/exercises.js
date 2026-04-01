fd0d79d21dfb922f99a620f90c8611909e05ac14
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2026-04-01 14:14:09 -0400

    Create exercises.js

---
+++ data/exercises.js
@@ -0,0 +1,20 @@
+export const exercises = [
+  {
+    id: '1',
+    name: 'Push Ups',
+    type: 'reps',
+    suggested: '2'
+  },
+  {
+    id: '2',
+    name: 'Jump Rope',
+    type: 'duration',
+    suggested: '1'
+  },
+  {
+    id: '3',
+    name: 'Sit Ups',
+    type: 'reps',
+    suggested: '2'
+  }
+];
